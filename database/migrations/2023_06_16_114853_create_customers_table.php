9b52bd631cedeefc89733ea43fe2dcef652b50ed
Lagbalu Lateef <chelseg14@gmail.com>
2023-06-16 17:30:50 +0100

    first commit

---
+++ database/migrations/2023_06_16_114853_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('name');
+            $table->enum('type',['individual', 'business']); //Individual or Company
+            $table->string('email')->unique();
+            $table->string('address');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class,'home']);
+Route::get('/about', [HomeController::class, 'about']);
+
+Route::get('/store', function(){
+
+    $category = request('category');
+    return 'You are viewing the store category of ' .$category;
+});
